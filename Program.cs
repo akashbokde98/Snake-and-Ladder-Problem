954267814c6bcffeb15a6e16433c6792966369e1
akashbokde98 <akashbokde98@gmail.com>
2023-01-29 21:58:42 +0530

    Added: Check Option No Play, Ladder or Snake

--- Program.cs
+++ Program.cs
@@ -8,8 +8,27 @@ namespace SnakeLadder
         {
             Console.WriteLine("Snake and Ladder Game Played With Single Player at Start Position 0.");
             int playerPosition = 0;
+
             Random rndNum = new Random();
             int dieNum = rndNum.Next(1, 7);
+
+            Random rndNum2 = new Random();
+            int checkOpt = rndNum2.Next(0, 3);
+
+            switch (checkOpt)
+            {
+                case 1:
+                    playerPosition += dieNum;
+                    break;
+
+                case 2:
+                    playerPosition += dieNum - dieNum;
+                    break;
+
+                default:
+                    Console.WriteLine("No Play");
+                    break;
+            }
         }
     }
 }
