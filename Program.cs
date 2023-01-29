bba15fc55df5dcef8228d1cdd305416bfe9edd2c
akashbokde98 <akashbokde98@gmail.com>
2023-01-29 20:45:01 +0530

    Added: Rolls the Die to get Number

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace SnakeLadder
         {
             Console.WriteLine("Snake and Ladder Game Played With Single Player at Start Position 0.");
             int playerPosition = 0;
+            Random rndNum = new Random();
+            int dieNum = rndNum.Next(1, 7);
         }
     }
 }
