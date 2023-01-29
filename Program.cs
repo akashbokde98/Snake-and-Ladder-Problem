af7e052754106acc9c49ae8257efa534731695fc
akashbokde98 <akashbokde98@gmail.com>
2023-01-30 04:08:36 +0530

    Added:  Winner

--- Program.cs
+++ Program.cs
@@ -6,39 +6,78 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Snake and Ladder Game Played With Single Player at Start Position 0.");
-            int playerPosition = 0;
-            int winPosition = 100;
-            int diceTimes = 0;
-
             Random rndNum = new Random();
-            Random rndNum2 = new Random();
+            Console.WriteLine("Snake and Ladder Game Played With Single Player at Start Position 0.");
+            int player1_Position = 0;
+            int player2_Position = 0;
+            int winPosition = 100;
 
-            while (playerPosition < winPosition)
+            while (player1_Position < winPosition && player2_Position < winPosition)
             {
-                int dieNum = rndNum.Next(1, 7);
-                diceTimes++;
-                switch (rndNum2.Next(0, 3))
+                int dieNum1 = rndNum.Next(1, 7);
+                int player1_Opt = rndNum.Next(0, 3);
+                int dieNum2 = rndNum.Next(1, 7);
+                int player2_Opt = rndNum.Next(0, 3);
+
+                if (dieNum1 > dieNum2)
                 {
-                    case 1:
-                        if ( ( playerPosition < 100 ) && ( playerPosition + dieNum <= 100 ) )
+                    if (player1_Opt == 1)
+                    {
+                        if ((player1_Position < 100) && (player1_Position + dieNum1 <= 100))
                         {
-                            playerPosition += dieNum;
-                            Console.WriteLine("playerPosition:" + playerPosition);
+                            player1_Position += dieNum1;
+                            Console.WriteLine("player1 Position:" + player1_Position);
                         }
-                        break;
+                        continue;
+                    }
+                    else
+                    {
+                        if (player1_Opt == 2)
+                        {
+                            player1_Position += dieNum1 - dieNum1;
+                            Console.WriteLine("player1 Position:" + player1_Position);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No Play");
+                        }
 
-                    case 2:
-                        playerPosition += dieNum - dieNum;
-                        Console.WriteLine("playerPosition:" + playerPosition);
-                        break;
+                    }
+                }
+                else
+                {
+                    if (player2_Opt == 1)
+                    {
+                        if ((player2_Position < 100) && (player2_Position + dieNum2 <= 100))
+                        {
+                            player2_Position += dieNum2;
+                            Console.WriteLine("player2 Position:" + player2_Position);
+                        }
+                        continue;
+                    }
+                    else
+                    {
+                        if (player2_Opt == 2)
+                        {
+                            player2_Position += dieNum2 - dieNum2;
+                            Console.WriteLine("player2 Position:" + player2_Position);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No Play");
+                        }
 
-                    default:
-                        Console.WriteLine("No Play");
-                        break;
+                    }
                 }
             }
-            Console.WriteLine(diceTimes + " Times Dice Was Played To Win the Game.");
+            if (player1_Position > player2_Position)
+            {
+                Console.WriteLine("Player_1 is Winner!");
+            }
+            else
+            {
+                Console.WriteLine("Player_2 is Winner!");
+            }
         }
     }
 }
