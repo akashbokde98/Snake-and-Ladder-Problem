6cbb478d52adc0db335e8b95047297257bb65ee8
akashbokde98 <akashbokde98@gmail.com>
2023-01-29 23:44:51 +0530

    Added: Player Reaches the Winnig Position

--- Program.cs
+++ Program.cs
@@ -8,26 +8,32 @@ namespace SnakeLadder
         {
             Console.WriteLine("Snake and Ladder Game Played With Single Player at Start Position 0.");
             int playerPosition = 0;
+            int winPosition = 100;
 
             Random rndNum = new Random();
             int dieNum = rndNum.Next(1, 7);
 
             Random rndNum2 = new Random();
-            int checkOpt = rndNum2.Next(0, 3);
+            //int checkOpt = rndNum2.Next(0, 3);
 
-            switch (checkOpt)
+            while (playerPosition < winPosition)
             {
-                case 1:
-                    playerPosition += dieNum;
-                    break;
+                switch (rndNum2.Next(0, 3))
+                {
+                    case 1:
+                        playerPosition += dieNum;
+                        Console.WriteLine("playerPosition:" + playerPosition);
+                        break;
 
-                case 2:
-                    playerPosition += dieNum - dieNum;
-                    break;
+                    case 2:
+                        playerPosition += dieNum - dieNum;
+                        Console.WriteLine("playerPosition:" + playerPosition);
+                        break;
 
-                default:
-                    Console.WriteLine("No Play");
-                    break;
+                    default:
+                        Console.WriteLine("No Play");
+                        break;
+                }
             }
         }
     }
